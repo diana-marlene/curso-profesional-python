93fa6da6d4225a7713911090af1022b2af3f4141
Diana Marlene <diana.sanchez9907@gmail.com>
2021-10-27 22:33:45 -0500

    bloque 1 python

---
+++ Bloque-1/1-variables.py
@@ -0,0 +1,21 @@
+titulo_curso= 'Curso profesional de Python'
+nombre_completo = 'Diana Sánchez'
+curso = "Curso Python"
+
+print(titulo_curso)
+
+# <nombre> = <valor>   Nombre descriptivo
+# Se ejecuta de forma descendente
+# Nomenclatura _ para unir dos palabras
+# Variables en minusculas
+
+# PALABRAS RESERVADAS: 
+"""
+False	class	is	return
+None	continue	lambda	try
+True	def	nonlocal	while
+and	del	not	with
+as	elif	or	yield
+assert	else	pass	*
+break	except	raise	*
+"""
---
+++ Bloque-1/2-constantes.py
@@ -0,0 +1,3 @@
+TITULO_CURSO = 'Curso profesional de Python'
+print(TITULO_CURSO)
+# Convencion de constantes en mayusculas y solo para lectura
